83e55a5bcc268459d68d1d0ffa2686ace1b8718f
Jason LaFrance <j-lafrance@weirdness.com>
2014-05-22 16:39:59 -0400

    Added Javadoc.

--- WTBBackend/src/com/jasonlafrance/wtbbackend/gtfs/StopTime.java
+++ WTBBackend/src/com/jasonlafrance/wtbbackend/gtfs/StopTime.java
@@ -15,7 +15,7 @@ This file is part of WTBBackend.
 
     You should have received a copy of the GNU General Public License
     along with WTBBackend.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package com.jasonlafrance.wtbbackend.gtfs;
 
@@ -27,286 +27,468 @@ import java.util.ArrayList;
 import com.jasonlafrance.wtbbackend.wtb_util.CSVParser;
 
 /**
- *
+ * GTFS StopTime class
+ * 
  * @author Jason LaFrance
  */
 public final class StopTime extends GTFSParser implements Comparable<StopTime> {
 
-    private static final String _filename = "//stop_times.txt";
+	private static final String _filename = "//stop_times.txt";
 
-    private String _trip_id = null;
-    //private String _arrival_time = "";
-    private int _arrivalTimecode = -1;
-    //private String _departure_time = "";
-    private int _departureTimecode = -1;
+	private String _trip_id = null;
+	// private String _arrival_time = "";
+	private int _arrivalTimecode = -1;
+	// private String _departure_time = "";
+	private int _departureTimecode = -1;
 
-    private String _stop_id = null;
-    private int _stop_sequence = -1;
-    private String _stop_headsign = null;
-    private int _pickup_type = -1;
-    private int _drop_off_type = -1;
-    private double _shape_dist_traveled = 0.0;
-    private String _timepoint = null;
-    private String _continuous_stops = null;
+	private String _stop_id = null;
+	private int _stop_sequence = -1;
+	private String _stop_headsign = null;
+	private int _pickup_type = -1;
+	private int _drop_off_type = -1;
+	private double _shape_dist_traveled = 0.0;
+	private String _timepoint = null;
+	private String _continuous_stops = null;
 
-    public StopTime() {;
-    }
+	/**
+	 * Stub constructor
+	 */
+	public StopTime() {
+	}
 
-    public StopTime(
-            String inTripID,
-            String inArrival,
-            String inDepart,
-            String inStopID,
-            int inStopSequence,
-            String inHeadsign,
-            int inPickupType,
-            int inDropoffType,
-            double inDistTraveled
-    ) {
-        _trip_id = inTripID;
-        //_arrival_time = inArrival;
-        _arrivalTimecode = timeToMinutes(inArrival);
-        //_departure_time = inDepart;
-        _departureTimecode = timeToMinutes(inDepart);
-        _stop_id = inStopID;
-        _stop_sequence = inStopSequence;
-        _stop_headsign = inHeadsign;
-        _pickup_type = inPickupType;
-        _drop_off_type = inDropoffType;
-        _shape_dist_traveled = inDistTraveled;
-    }
+	/**
+	 * Stub constructor
+	 * 
+	 * @param inLine
+	 *            A String
+	 */
+	public StopTime(String inLine) {
+	}
 
-    public StopTime(String inLine) {
+	// do this without reflection!
+	/**
+	 * Create a StopTime object with a supplied line from a GTFS stop_times.txt
+	 * table.
+	 * 
+	 * @param inLine
+	 *            Line from a GTFS stop_times.txt table.
+	 * @param id
+	 *            The GTFS ID from the GTFS multition that this object belongs
+	 *            to.
+	 */
 
-    }
+	public StopTime(String inLine, int id) {
+		ArrayList<String> h = _headers.get(id).get(_filename);
+		String[] f = CSVParser.parseLine(inLine);
 
-    // do this without reflection!
-    public StopTime(String inLine, int id) {
-        ArrayList<String> h = _headers.get(id).get(_filename);
-        String[] f = CSVParser.parseLine(inLine);
+		if (f.length != h.size()) {
+			return;
+		}
 
-        if (f.length != h.size()) {
-            return;
-        }
+		for (int i = 0; i < f.length; i++) {
+			switch (h.get(i)) {
+			case "arrival_time":
+				set_arrival_time(f[i]);
+				break;
+			case "continuous_stops":
+				set_continuous_stops(f[i]);
+				break;
+			case "departure_time":
+				set_departure_time(f[i]);
+				break;
+			case "drop_off_type":
+				set_drop_off_type(f[i]);
+				break;
+			case "pickup_type":
+				set_pickup_type(f[i]);
+				break;
+			case "shape_dist_traveled":
+				set_shape_dist_traveled(f[i]);
+				break;
+			case "stop_headsign":
+				set_stop_headsign(f[i]);
+				break;
+			case "stop_id":
+				set_stop_id(f[i]);
+				break;
+			case "stop_sequence":
+				set_stop_sequence(f[i]);
+				break;
+			case "timepoint":
+				set_timepoint(f[i]);
+				break;
+			case "trip_id":
+				set_trip_id(f[i]);
+				break;
+			default:
+				break;
+			}
+		}
+	}
 
-        for (int i = 0; i < f.length; i++) {
-            switch (h.get(i)) {
-                case "arrival_time":
-                    set_arrival_time(f[i]);
-                    break;
-                case "continuous_stops":
-                    set_continuous_stops(f[i]);
-                    break;
-                case "departure_time":
-                    set_departure_time(f[i]);
-                    break;
-                case "drop_off_type":
-                    set_drop_off_type(f[i]);
-                    break;
-                case "pickup_type":
-                    set_pickup_type(f[i]);
-                    break;
-                case "shape_dist_traveled":
-                    set_shape_dist_traveled(f[i]);
-                    break;
-                case "stop_headsign":
-                    set_stop_headsign(f[i]);
-                    break;
-                case "stop_id":
-                    set_stop_id(f[i]);
-                    break;
-                case "stop_sequence":
-                    set_stop_sequence(f[i]);
-                    break;
-                case "timepoint":
-                    set_timepoint(f[i]);
-                    break;
-                case "trip_id":
-                    set_trip_id(f[i]);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+	/**
+	 * Create a StopTime object from given values
+	 * 
+	 * @param inTripID
+	 *            Trip ID
+	 * @param inArrival
+	 *            Arrival time code
+	 * @param inDepart
+	 *            Departure time code
+	 * @param inStopID
+	 *            Stop ID
+	 * @param inStopSequence
+	 *            Stop sequence number
+	 * @param inHeadsign
+	 *            Headsign text
+	 * @param inPickupType
+	 *            Pickup type
+	 * @param inDropoffType
+	 *            Drop off type
+	 * @param inDistTraveled
+	 *            Distance traveled in trip
+	 */
+	public StopTime(String inTripID, String inArrival, String inDepart,
+			String inStopID, int inStopSequence, String inHeadsign,
+			int inPickupType, int inDropoffType, double inDistTraveled) {
+		_trip_id = inTripID;
+		// _arrival_time = inArrival;
+		_arrivalTimecode = timeToMinutes(inArrival);
+		// _departure_time = inDepart;
+		_departureTimecode = timeToMinutes(inDepart);
+		_stop_id = inStopID;
+		_stop_sequence = inStopSequence;
+		_stop_headsign = inHeadsign;
+		_pickup_type = inPickupType;
+		_drop_off_type = inDropoffType;
+		_shape_dist_traveled = inDistTraveled;
+	}
 
-    public void set_trip_id(String in) {
-        _trip_id = in;
-    }
+	@Override
+	public int compareTo(StopTime o) {
+		// compare _trip_id
+		int ret = _trip_id.compareTo(o.get_trip_id());
+		if (ret == 0) {
+			// compare _stop_sequence
+			if (o.get_stop_sequence() < _stop_sequence) {
+				ret = 1;
+			}
+			if (o.get_stop_sequence() > _stop_sequence) {
+				ret = -1;
+			}
+		}
+		// if we make it here, then the two are congruent
+		return ret;
+	}
 
-    public void set_arrival_time(String in) {
+	/**
+	 * Get the arrival time
+	 * 
+	 * @return Arrival time
+	 */
+	public String get_arrival_time() {
+		return minutesToTime(_arrivalTimecode);
+	}
 
-        _arrivalTimecode = timeToMinutes(in);
+	/**
+	 * Get continuous stops
+	 * 
+	 * @return Continuous stops
+	 */
+	public String get_continuous_stops() {
+		return _continuous_stops;
+	}
 
-    }
+	/**
+	 * Get the departure time
+	 * 
+	 * @return Departure time
+	 */
+	public String get_departure_time() {
+		return minutesToTime(_departureTimecode);
+	}
 
-    public void set_departure_time(String in) {
+	/**
+	 * Get the drop off type
+	 * 
+	 * @return Drop off type
+	 */
+	public int get_drop_off_type() {
+		return _drop_off_type;
+	}
 
-        _departureTimecode = timeToMinutes(in);
+	/**
+	 * Get the pickup type
+	 * 
+	 * @return Pickup type
+	 */
+	public int get_pickup_type() {
+		return _pickup_type;
+	}
 
-    }
+	/**
+	 * Get distance traveled in trip
+	 * 
+	 * @return Distance traveled
+	 */
+	public double get_shape_dist_traveled() {
+		return _shape_dist_traveled;
+	}
 
-    public void set_stop_id(String in) {
-        _stop_id = in;
-    }
+	/**
+	 * Get stop headsign text
+	 * 
+	 * @return Headsign text
+	 */
+	public String get_stop_headsign() {
+		return _stop_headsign;
+	}
 
-    public void set_stop_sequence(int in) {
-        _stop_sequence = in;
-    }
+	/**
+	 * Get the stop ID
+	 * 
+	 * @return Stop ID
+	 */
+	public String get_stop_id() {
+		return _stop_id;
+	}
 
-    public void set_stop_sequence(String in) {
-        try {
-            _stop_sequence = Integer.parseInt(in);
-        } catch (NumberFormatException e) {;
-        }
-    }
+	/**
+	 * Get the stop sequence
+	 * 
+	 * @return Stop sequence
+	 */
+	public int get_stop_sequence() {
+		return _stop_sequence;
+	}
 
-    public void set_stop_headsign(String in) {
-        _stop_headsign = in;
-    }
+	/**
+	 * Get the time point
+	 * 
+	 * @return Time point
+	 */
+	public String get_timepoint() {
+		return _timepoint;
+	}
 
-    public void set_pickup_type(int in) {
-        _pickup_type = in;
-    }
+	/**
+	 * Get the trip ID
+	 * 
+	 * @return Trip ID
+	 */
+	public String get_trip_id() {
+		return _trip_id;
+	}
 
-    public void set_pickup_type(String in) {
-        try {
-            _pickup_type = Integer.parseInt(in);
-        } catch (NumberFormatException e) {;
-        }
-    }
+	/**
+	 * Get the arrival time code
+	 * 
+	 * @return Arrival time code
+	 */
+	public int getArrivalTimecode() {
+		return _arrivalTimecode;
+	}
 
-    public void set_drop_off_type(int in) {
-        _drop_off_type = in;
-    }
+	/**
+	 * Get the departure time code
+	 * 
+	 * @return Departure time code
+	 */
+	public int getDepartureTimecode() {
+		return _departureTimecode;
+	}
 
-    public void set_drop_off_type(String in) {
-        try {
-            _drop_off_type = Integer.parseInt(in);
-        } catch (NumberFormatException e) {;
-        }
-    }
+	/**
+	 * Get the GTFS file name associated with this object.
+	 * 
+	 * @return The GTFS file name associated with this object.
+	 */
+	@Override
+	public String getFilename() {
+		return _filename;
+	}
 
-    public void set_shape_dist_traveled(double in) {
-        _shape_dist_traveled = in;
-    }
+	/**
+	 * Get this objects unique ID.
+	 * 
+	 * @return This objects unique ID
+	 */
+	@Override
+	public int getID() {
+		return _trip_id.hashCode();
+	}
 
-    public void set_shape_dist_traveled(String in) {
-        try {
-            _shape_dist_traveled = Double.parseDouble(in);
-        } catch (NumberFormatException e) {;
-        }
-    }
+	/**
+	 * Set the arrival time
+	 * 
+	 * @param in
+	 *            Arrival time
+	 */
+	public void set_arrival_time(String in) {
+		_arrivalTimecode = timeToMinutes(in);
+	}
 
-    public String get_trip_id() {
-        return _trip_id;
-    }
+	/**
+	 * Set continuous stops
+	 * 
+	 * @param _continuous_stops
+	 *            The continuous stops value to set
+	 */
+	public void set_continuous_stops(String _continuous_stops) {
+		this._continuous_stops = _continuous_stops;
+	}
 
-    @Override
-    public int getID() {
-        return _trip_id.hashCode();
-    }
+	/**
+	 * Set the departure time
+	 * 
+	 * @param in
+	 *            Departure time
+	 */
+	public void set_departure_time(String in) {
+		_departureTimecode = timeToMinutes(in);
+	}
 
-    public String get_arrival_time() {
-        return minutesToTime(_arrivalTimecode);
-    }
+	/**
+	 * Set the drop off type
+	 * 
+	 * @param in
+	 *            Drop off type
+	 */
+	public void set_drop_off_type(int in) {
+		_drop_off_type = in;
+	}
 
-    public int getArrivalTimecode() {
-        return _arrivalTimecode;
-    }
+	/**
+	 * Set the drop off type
+	 * 
+	 * @param in
+	 *            Drop off type
+	 */
+	public void set_drop_off_type(String in) {
+		try {
+			_drop_off_type = Integer.parseInt(in);
+		} catch (NumberFormatException e) {
+			;
+		}
+	}
 
-    public String get_departure_time() {
-        return minutesToTime(_departureTimecode);
-    }
+	/**
+	 * Set the pickup type
+	 * 
+	 * @param in
+	 *            Pickup type
+	 */
+	public void set_pickup_type(int in) {
+		_pickup_type = in;
+	}
 
-    public int getDepartureTimecode() {
-        return _departureTimecode;
-    }
+	/**
+	 * Set the pickup type
+	 * 
+	 * @param in
+	 *            Pickup type
+	 */
+	public void set_pickup_type(String in) {
+		try {
+			_pickup_type = Integer.parseInt(in);
+		} catch (NumberFormatException e) {
+			;
+		}
+	}
 
-    public String get_stop_id() {
-        return _stop_id;
-    }
+	/**
+	 * Set distance traveled
+	 * 
+	 * @param in
+	 *            Distance
+	 */
+	public void set_shape_dist_traveled(double in) {
+		_shape_dist_traveled = in;
+	}
 
-    public int get_stop_sequence() {
-        return _stop_sequence;
-    }
+	/**
+	 * Set distance traveled
+	 * 
+	 * @param in
+	 *            Distance
+	 */
+	public void set_shape_dist_traveled(String in) {
+		try {
+			_shape_dist_traveled = Double.parseDouble(in);
+		} catch (NumberFormatException e) {
+			;
+		}
+	}
 
-    public String get_stop_headsign() {
-        return _stop_headsign;
-    }
+	/**
+	 * Set headsign text
+	 * 
+	 * @param in
+	 *            Headsign text
+	 */
+	public void set_stop_headsign(String in) {
+		_stop_headsign = in;
+	}
 
-    public int get_pickup_type() {
-        return _pickup_type;
-    }
+	/**
+	 * Set stop ID
+	 * 
+	 * @param in
+	 *            Stop ID
+	 */
+	public void set_stop_id(String in) {
+		_stop_id = in;
+	}
 
-    public int get_drop_off_type() {
-        return _drop_off_type;
-    }
+	/**
+	 * Set stop sequence order
+	 * 
+	 * @param in
+	 *            Sequence order
+	 */
+	public void set_stop_sequence(int in) {
+		_stop_sequence = in;
+	}
 
-    public double get_shape_dist_traveled() {
-        return _shape_dist_traveled;
-    }
+	/**
+	 * Set stop sequence order
+	 * 
+	 * @param in
+	 *            Sequence order
+	 */
+	public void set_stop_sequence(String in) {
+		try {
+			_stop_sequence = Integer.parseInt(in);
+		} catch (NumberFormatException e) {
+			;
+		}
+	}
 
-    @Override
-    public String toString() {
-        return _trip_id + ","
-                + minutesToTime(_arrivalTimecode) + ","
-                + minutesToTime(_arrivalTimecode) + ","
-                + _stop_id + ","
-                + _stop_sequence + ","
-                + _stop_headsign + ","
-                + _pickup_type + ","
-                + _drop_off_type + ","
-                + _shape_dist_traveled + "\n";
-    }
+	/**
+	 * Set time point
+	 * 
+	 * @param _timepoint
+	 *            The time point to set
+	 */
+	public void set_timepoint(String _timepoint) {
+		this._timepoint = _timepoint;
+	}
 
-    @Override
-    public int compareTo(StopTime o) {
-        // compare _trip_id
-        int ret = _trip_id.compareTo(o.get_trip_id());
-        if (ret == 0) {
-            // compare _stop_sequence
-            if (o.get_stop_sequence() < _stop_sequence) {
-                ret = 1;
-            }
-            if (o.get_stop_sequence() > _stop_sequence) {
-                ret = -1;
-            }
-        }
-        // if we make it here, then the two are congruent
-        return ret;
-    }
+	/**
+	 * Set the trip ID
+	 * 
+	 * @param in
+	 *            Trip ID
+	 */
+	public void set_trip_id(String in) {
+		_trip_id = in;
+	}
 
-    @Override
-    public String getFilename() {
-        return _filename;
-    }
-
-    /**
-     * @return the _timepoint
-     */
-    public String get_timepoint() {
-        return _timepoint;
-    }
-
-    /**
-     * @param _timepoint the _timepoint to set
-     */
-    public void set_timepoint(String _timepoint) {
-        this._timepoint = _timepoint;
-    }
-
-    /**
-     * @return the _continuous_stops
-     */
-    public String get_continuous_stops() {
-        return _continuous_stops;
-    }
-
-    /**
-     * @param _continuous_stops the _continuous_stops to set
-     */
-    public void set_continuous_stops(String _continuous_stops) {
-        this._continuous_stops = _continuous_stops;
-    }
+	@Override
+	public String toString() {
+		return _trip_id + "," + minutesToTime(_arrivalTimecode) + ","
+				+ minutesToTime(_arrivalTimecode) + "," + _stop_id + ","
+				+ _stop_sequence + "," + _stop_headsign + "," + _pickup_type
+				+ "," + _drop_off_type + "," + _shape_dist_traveled + "\n";
+	}
 }
--- WTBBackend/src/com/jasonlafrance/wtbbackend/gtfs/Transfer.java
+++ WTBBackend/src/com/jasonlafrance/wtbbackend/gtfs/Transfer.java
@@ -15,7 +15,7 @@ This file is part of WTBBackend.
 
     You should have received a copy of the GNU General Public License
     along with WTBBackend.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package com.jasonlafrance.wtbbackend.gtfs;
 
@@ -26,124 +26,217 @@ import java.util.ArrayList;
 import com.jasonlafrance.wtbbackend.wtb_util.CSVParser;
 
 /**
- *
+ * GTFS Transfer class
+ * 
  * @author Jason LaFrance
  */
 public final class Transfer extends GTFSParser {
 
-    private static final String _filename = "//transfers.txt";
+	private static final String _filename = "//transfers.txt";
 
-    private String _from_stop_id = null, _to_stop_id = null;
-    private int _transfer_type = -1;
-    private double _min_transfer_time = 0.0;
+	private String _from_stop_id = null, _to_stop_id = null;
+	private int _transfer_type = -1;
+	private double _min_transfer_time = 0.0;
 
-    public Transfer() {;
-    }
+	/**
+	 * Stub constructor
+	 */
+	public Transfer() {
+	}
 
-    public Transfer(
-            String inFrom,
-            String inTo,
-            int inType,
-            double minTrans
-    ) {
-        _from_stop_id = inFrom;
-        _to_stop_id = inTo;
-        _transfer_type = inType;
-        _min_transfer_time = minTrans;
-    }
+	/**
+	 * Stub constructor
+	 * 
+	 * @param inLine
+	 *            A String
+	 */
+	public Transfer(String inLine) {
+	}
 
-    public Transfer(String inLine) {
-    }
-    
-    public Transfer(String inLine, int id) {
-        ArrayList<String> h = _headers.get(id).get(_filename);
-        String[] f = CSVParser.parseLine(inLine);
+	/**
+	 * Create a Transfer object with a supplied line from a GTFS transfers.txt
+	 * table.
+	 * 
+	 * @param inLine
+	 *            Line from a GTFS transfers.txt table.
+	 * @param id
+	 *            The GTFS ID from the GTFS multition that this object belongs
+	 *            to.
+	 */
+	public Transfer(String inLine, int id) {
+		ArrayList<String> h = _headers.get(id).get(_filename);
+		String[] f = CSVParser.parseLine(inLine);
 
-        if (f.length != h.size()) {
-            return;
-        }
+		if (f.length != h.size()) {
+			return;
+		}
 
-        for (int i = 0; i < f.length; i++) {
-            switch (h.get(i)) {
-                case "from_stop_id":
-                    set_from_stop_id(f[i]);
-                    break;
-                case "min_transfer_time":
-                    set_min_transfer_time(f[i]);
-                    break;
-                case "to_stop_id":
-                    set_to_stop_id(f[i]);
-                    break;
-                case "transfer_type":
-                    set_transfer_type(f[i]);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+		for (int i = 0; i < f.length; i++) {
+			switch (h.get(i)) {
+			case "from_stop_id":
+				set_from_stop_id(f[i]);
+				break;
+			case "min_transfer_time":
+				set_min_transfer_time(f[i]);
+				break;
+			case "to_stop_id":
+				set_to_stop_id(f[i]);
+				break;
+			case "transfer_type":
+				set_transfer_type(f[i]);
+				break;
+			default:
+				break;
+			}
+		}
+	}
 
-    public void set_from_stop_id(String in) {
-        _from_stop_id = in;
-    }
+	/**
+	 * Create a Transfer object from given values
+	 * 
+	 * @param inFrom
+	 *            Stop ID coming from
+	 * @param inTo
+	 *            Stop ID going to
+	 * @param inType
+	 *            Transfer type
+	 * @param minTrans
+	 *            Minimum transfer time
+	 */
+	public Transfer(String inFrom, String inTo, int inType, double minTrans) {
+		_from_stop_id = inFrom;
+		_to_stop_id = inTo;
+		_transfer_type = inType;
+		_min_transfer_time = minTrans;
+	}
 
-    public void set_to_stop_id(String in) {
-        _to_stop_id = in;
-    }
+	/**
+	 * Get ID of stop coming from
+	 * 
+	 * @return From stop ID
+	 */
+	public String get_from_stop_id() {
+		return _from_stop_id;
+	}
 
-    public void set_transfer_type(int in) {
-        _transfer_type = in;
-    }
+	/**
+	 * Get minimum transfer time
+	 * 
+	 * @return Minimum transfer time
+	 */
+	public double get_min_transfer_time() {
+		return _min_transfer_time;
+	}
 
-    public void set_transfer_type(String in) {
-        try {
-            _transfer_type = Integer.parseInt(in);
-        } catch (NumberFormatException e) {;
-        }
-    }
+	/**
+	 * Get ID of stop going to
+	 * 
+	 * @return To stop ID
+	 */
+	public String get_to_stop_id() {
+		return _to_stop_id;
+	}
 
-    public void set_min_transfer_time(double in) {
-        _min_transfer_time = in;
-    }
+	/**
+	 * Get transfer tpe
+	 * 
+	 * @return Transfer type
+	 */
+	public int get_transfer_type() {
+		return _transfer_type;
+	}
 
-    public void set_min_transfer_time(String in) {
-        try {
-            _min_transfer_time = Double.parseDouble(in);
-        } catch (NumberFormatException e) {;
-        }
-    }
+	/**
+	 * Get the GTFS file name associated with this object.
+	 * 
+	 * @return The GTFS file name associated with this object.
+	 */
+	@Override
+	public String getFilename() {
+		return _filename;
+	}
 
-    @Override
-    public int getID() {
-        return _from_stop_id.hashCode();
-    }
+	/**
+	 * Get this objects unique ID.
+	 * 
+	 * @return This objects unique ID
+	 */
+	@Override
+	public int getID() {
+		return _from_stop_id.hashCode();
+	}
 
-    public String get_from_stop_id() {
-        return _from_stop_id;
-    }
+	/**
+	 * Set stop ID coming from
+	 * 
+	 * @param in
+	 *            From stop ID
+	 */
+	public void set_from_stop_id(String in) {
+		_from_stop_id = in;
+	}
 
-    public String get_to_stop_id() {
-        return _to_stop_id;
-    }
+	/**
+	 * Set minimum transfer time
+	 * 
+	 * @param in
+	 *            Minimum transfer time
+	 */
+	public void set_min_transfer_time(double in) {
+		_min_transfer_time = in;
+	}
 
-    public int get_transfer_type() {
-        return _transfer_type;
-    }
+	/**
+	 * Set minimum transfer time
+	 * 
+	 * @param in
+	 *            Minimum transfer time
+	 */
+	public void set_min_transfer_time(String in) {
+		try {
+			_min_transfer_time = Double.parseDouble(in);
+		} catch (NumberFormatException e) {
+			;
+		}
+	}
 
-    public double get_min_transfer_time() {
-        return _min_transfer_time;
-    }
+	/**
+	 * Set stop ID going to next
+	 * 
+	 * @param in
+	 *            To stop ID
+	 */
+	public void set_to_stop_id(String in) {
+		_to_stop_id = in;
+	}
 
-    @Override
-    public String toString() {
-        return _from_stop_id + ","
-                + _to_stop_id + ","
-                + _transfer_type + ","
-                + _min_transfer_time + "\n";
-    }
+	/**
+	 * Set transfer type
+	 * 
+	 * @param in
+	 *            Transfer type
+	 */
+	public void set_transfer_type(int in) {
+		_transfer_type = in;
+	}
 
-    @Override
-    public String getFilename() {
-        return _filename;
-    }
+	/**
+	 * Set transfer type
+	 * 
+	 * @param in
+	 *            Transfer type
+	 */
+	public void set_transfer_type(String in) {
+		try {
+			_transfer_type = Integer.parseInt(in);
+		} catch (NumberFormatException e) {
+			;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return _from_stop_id + "," + _to_stop_id + "," + _transfer_type + ","
+				+ _min_transfer_time + "\n";
+	}
 }
